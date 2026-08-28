2ebf2edadb2d137b3d32424cdbdd052681cf8f4d
Peter Vigier <3204560+p3t@users.noreply.github.com>
2026-08-28 10:31:58 +0200

    Fixes #272: Behaviour of firstFilterWith (#280)
    The current implementation was not useful, as it would always return the top-level list and never the concrete Filter.
    Modified to return the Filter instance also expressing this with method signature change.
    Added additional search method to return the directly containing list of the specified filter-attribute

--- cursorpaging-jpa/src/test/java/io/vigier/cursorpaging/jpa/PageRequestTest.java
+++ cursorpaging-jpa/src/test/java/io/vigier/cursorpaging/jpa/PageRequestTest.java
@@ -1,6 +1,7 @@
 package io.vigier.cursorpaging.jpa;
 
-import io.vigier.cursorpaging.jpa.filter.AndFilter;
+import io.vigier.cursorpaging.jpa.filter.FilterType;
+import io.vigier.cursorpaging.jpa.filter.OrFilter;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
@@ -8,6 +9,7 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.InstanceOfAssertFactories.type;
 
 class PageRequestTest {
 
@@ -16,7 +18,8 @@ class PageRequestTest {
     @ValueSource( strings = { "  ", "\t", "\n" } )
     void shouldIgnoreEmptyFilter( final String value ) {
         final var pageRequest = PageRequest.create( b -> b.asc( Attribute.of( "id", Long.class ) )
-                .filter( Filter.create( f -> f.attribute( Attribute.of( "test", String.class ) ).equalTo( value ) ) ) );
+                .filter( Filter.create( f -> f.attribute( Attribute.of( "test", String.class ) )
+                        .equalTo( value ) ) ) );
 
         assertThat( pageRequest.filters() ).isEmpty();
     }
@@ -24,41 +27,62 @@ class PageRequestTest {
     @Test
     void shouldAddPositionAndFilterIfValuePresent() {
         final var pageRequest = PageRequest.create( b -> b.asc( Attribute.of( "id", Long.class ) )
-                .filter( Filters.attribute( "test", String.class ).equalTo( "value" ) ) );
+                .filter( Filters.attribute( "test", String.class )
+                        .equalTo( "value" ) ) );
 
         assertThat( pageRequest.filters() ).hasSize( 1 );
-        assertThat( pageRequest.positions() ).hasSize( 1 ).first().satisfies( p -> {
-            assertThat( p.attribute().name() ).isEqualTo( "id" );
-            assertThat( p.order() ).isEqualTo( Order.ASC );
-        } );
+        assertThat( pageRequest.positions() ).hasSize( 1 )
+                .first()
+                .satisfies( p -> {
+                    assertThat( p.attribute()
+                            .name() ).isEqualTo( "id" );
+                    assertThat( p.order() ).isEqualTo( Order.ASC );
+                } );
     }
 
     @Test
     void shouldFailWhenRequestWithoutOrder() {
-        assertThatThrownBy( () -> PageRequest.create(
-                b -> b.filter( Filters.attribute( "test", String.class ).equalTo( "value" ) ) ) ).isInstanceOf(
-                        IllegalArgumentException.class )
+        assertThatThrownBy( () -> PageRequest.create( b -> b.filter( Filters.attribute( "test", String.class )
+                .equalTo( "value" ) ) ) ).isInstanceOf( IllegalArgumentException.class )
                 .hasMessageContaining(
                         "at least one order-attribute (asc/desc) for determine the position of the page start is required" );
     }
 
     @Test
     void shouldFindFilterByAttribute() {
-        final var pageRequest = PageRequest.create( b -> b.asc( Attribute.of( "id", Long.class ) )
-                .filter( Filters.attribute( "test", String.class ).equalTo( "value" ) )
-                .filter( Filters.and( Filters.attribute( "test", String.class ).equalTo( "value2" ),
-                        Filters.attribute( "test2", String.class ).equalTo( "value3" ) ) ) );
+        final var test1Attr = Attribute.of( "test", String.class );
+        final var test2Attr = Attribute.of( "test2", String.class );
+        final var test3Attr = Attribute.of( "test3", String.class );
 
-        assertThat( pageRequest.firstFilterWith( Attribute.of( "test", String.class ) ) ).isPresent();
+        final var pageRequest = PageRequest.create( b -> b.asc( Attribute.of( "id", Long.class ) )
+                .filter( Filters.attribute( test1Attr )
+                        .equalTo( "value" ) )
+                .filter( Filters.or( Filters.attribute( test2Attr )
+                                .equalTo( "value2" ),  //
+                        Filters.attribute( test3Attr )
+                                .equalTo( "value3" ) ) ) );
+
+        assertThat( pageRequest.firstFilterWith( test1Attr ) ).isPresent();
         assertThat( pageRequest.firstFilterWith( Attribute.of( "test", Long.class ) ) ).isEmpty();
-        assertThat( pageRequest.firstFilterWith( Attribute.of( "test2", String.class ) ) ).isPresent()
+
+        assertThat( pageRequest.firstFilterWith( test3Attr ) ).isPresent()
                 .get()
-                .isInstanceOf( AndFilter.class );
+                .asInstanceOf( type( Filter.class ) )
+                .satisfies( f -> {
+                    assertThat( f.attribute() ).isEqualTo( test3Attr );
+                    assertThat( f.operation() ).isEqualTo( FilterType.EQUAL_TO );
+                } );
+
+        assertThat( pageRequest.firstFilterListWith( test3Attr ) ).isPresent()
+                .get()
+                .asInstanceOf( type( OrFilter.class ) )
+                .satisfies( f -> assertThat( f.attributes() ).contains( test2Attr, test3Attr ) );
     }
 
     @Test
     void shouldAcceptNullAsFilter() {
-        final var pageRequest = PageRequest.create( b -> b.asc( Attribute.of( "id", Long.class ) ).filter( null ) );
+        final var pageRequest = PageRequest.create( b -> b.asc( Attribute.of( "id", Long.class ) )
+                .filter( null ) );
 
         assertThat( pageRequest.filters() ).isEmpty();
     }
